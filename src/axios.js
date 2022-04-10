97d81af20711e0f83b7e004d243ecf685ce08f87
Derrick Ampire <ampire90@gmail.com>
2022-04-10 17:25:04 +0300

    base URL added

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // creates cloud function with base URL
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/drug-store-9dcfb/us-central1/api' // Cloud function URL
+    baseURL: 'https://us-central1-drug-store-9dcfb.cloudfunctions.net/api'
+    // 'http://localhost:5001/drug-store-9dcfb/us-central1/api' // Cloud function URL
 });
 
 export default instance;
