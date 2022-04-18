81189ab17265d9ad1fe4a43a98bcbd981f010a4e
Derrick Ampire <ampire90@gmail.com>
2022-04-18 21:05:23 +0300

    Indentation fixed

--- src/Order.js
+++ src/Order.js
@@ -26,7 +26,7 @@ function Order({ order }) {
         <CurrencyFormat
             renderText={(value) => (
                 <h3 className='order__total'>Order Total: {value}</h3>
-                )}
+            )}
             decimalScale={2}
             value={order.data.amount / 100}
             displayType={'text'}
